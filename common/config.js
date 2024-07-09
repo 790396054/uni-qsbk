f39dfc3815dc6d14479b85553b9fed4ff26c5040
gmm <790396054@qq.com>
2024-07-09 21:53:07 +0800

    init

---
+++ common/config.js
@@ -0,0 +1,10 @@
+// 配置信息
+export default {
+	// api请求前缀
+	// webUrl:'https://api.ceshi2.dishait.cn/api/v1',
+	webUrl: 'https://ceshi2.dishait.cn/api/v1',
+	// websocket地址
+	websocketUrl:"wss://api.yuchentt.com/wss",
+	// 消息提示tabbar索引
+	TabbarIndex:2
+}
